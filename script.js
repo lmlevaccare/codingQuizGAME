5c86557bd1b5476c23608977aa47c6b72b84679d
lauren levaccare <laurenlevaccare@icloud.com>
2020-11-24 12:20:25 -0600

    added working questions and timer

--- script.js
+++ script.js
@@ -1,197 +1,135 @@
 
-var secondsLeft = 60;
 
-var pntCard = document.getElementById("scoreCard");
+const questionContainerElement = document.getElementById('questionContainer')
+var choicesContainer = document.getElementById("multiChoice")
+
+var btnA = document.getElementById("btnA");
+var btnB = document.getElementById("btnB");
+var btnC = document.getElementById("btnC");
+
 
 var indexCorrect = []
-var ansBTn = document.querySelectorAll("ansBTN");
-// var nxtBtn = document.getElementById("nxtBtn");
-// var startBtn = document.getElementById("stBtn");
-// var inputs = document.getElementById("start")
-var questionContainer = document.getElementById("questionContain");
 
-
-var count = 0;
 var pntCard = document.querySelector("#scoreCard");
-// var decrementEl = document.querySelector("#decrement");
-var countER= document.querySelector("#count");
 
-var choicesContainer = document.getElementById("multiChoice")
-var choiceArray0 = document.getElementById("choiceA");
-var choiceArray1 = document.getElementById ("choiceB");
-var choiceArray2= document.getElementById("choiceC");
+var countER = document.querySelector("#count");
+
+// var start = document.getElementById('start');
 
 
+
+
+var questiondiv = document.getElementById("questionS");
+
+
+ var nextBtn = document.getElementById('add');
+// nextBtn.addEventListener("click", addQuestion);
+
+
+
+var secondsLeft = 60;
+
+var questionStart = "what is html?"
+  var choices = ["mark-up", "stella", "lauren"];
 
 function myStart() {
+   
+   
 
-var timerInterval = setInterval(function() {
-secondsLeft--;
-start.textContent = secondsLeft;
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
 
-if(secondsLeft === 0) {
-clearInterval(timerInterval);
-alert("Game-Over");
-}
-
-         
-}, 1000);
-
-  questionS.innerHTML = question1.questionContainer;
-  choiceA.innerHTML = question1.choicesContainer[0];
-  choiceB.innerHTML = question1.choicesContainer[1];
-  choiceC.innerHTML = question1.choicesContainer[2];
-}
-
-
-// QUESTIONS
-
-let question1 = {
-questionContainer: "Where is the text of the title tag displayed?",
-choicesContainer: ["title bar", "body tag", "nav bar"],
-// indexCorrect: 0
-}
-
-let question2 = {
-questionContainer: "what is Java Source Code is compiled into?",
-choicesContainer: ["Bytecode", "eXcode", "IDK"],
-// indexCorrect: 0
-}
-
-
-let question3 = {
-questionContainer: "How can you remove an item by index position?",
-choicesContainer: ["Splice", "hoP", "Jump"],
-// indexCorrect: 0
-}
-
-//  START BTN ACTIVATED TIMER
-
-
-
-
-function nxtQuest() {
-  questionS.innerHTML = question2.questionContainer;
-  choiceA.innerHTML = question2.choicesContainer[0];
-  choiceB.innerHTML = question2.choicesContainer[1];
-  choiceC.innerHTML = question2.choicesContainer[2];
-  
-}
-
-function setCounterText() {
-  countER.textContent = count;
-}
-
-choiceA.addEventListener("click", function() {
-  count++;
-  setCounterText();
-});
-
-choiceB.addEventListener("click", function() {
-  if(count > 0) {
-    count--;
-    setCounterText();
-  }
-
-
-
-  
-}); 
  
+        // $('#start').text(secondsLeft)
+        start.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert("Game-Over");
+        }
+
+
+    }, 1000);
+
+
+    $('#questionS').append('<p>' + questionStart + '</p>');
+    $('#multiChoiceA').append('<p>' + choices[0] + '</p>');
+    $('#multiChoiceB').append('<p>' + choices[1] + '</p>');
+    $('#multiChoiceC').append('<p>' + choices[2] + '</p>');
+
+    $('#startBtn').hide()
 
 
 
-// function setCounterText() {
-
-// }
-
-// incrementEl.addEventListener("click", function() {
-//   count++;
-//   setCounterText();
-// });
-
-// decrementEl.addEventListener("click", function() {
-//   if(count > 0) {
-//     count--;
-//     setCounterText();
-//   }
-// }); 
+}
 
 
-// startBtn.addEventListener("click",myStart());
 
-// WHEN A USER CLICKS ON THE BUTTN WITH THE CLASS ANS BTN  
-// IS THE USER RIGHT OR WRONG
-// IF RIGHT GET PNT AND ADV
-// IF WRONG MINUS TIMECLK  secondsleft minus 5 AND ADV 
 
-// ADD ARRAY OF ANS OBJECT
-// GET NEXT QUEST - ARRAY OF QUEST OBJ
+
+
+let shuffledQuestions, currentQuestionIndex
+
+
+
+nextBtn.addEventListener('click', function (entries) {
+      
+    entries =questionArray
+   
+shuffledQuestions = entries.sort(() => Math.random() - .5)
+        currentQuestionIndex = 0
+    entries = shuffledQuestions[currentQuestionIndex]
+    
 
     
-// QUIZ QUESTIONS
-
-// var userQuess = function myChoice() {
-
-//   if (userQuess ===) {
-
-//   }
-
-// }
+    entries=JSON.stringify(entries)
+        $("#questionS").text(entries)
+    $("#multiChoiceA").text(answerArrayA[0])
+    $("#multiChoiceB").text(answerArray1[1])
+     $("#multiChoiceC").text(answerArray1[2])
+    
 
 
-
-
-
-// var answersFalse = []
+    });
 
 
 
 
-// // quest.textContent = question1.quest1;
-// anwsersF.textContent = question1.choice1;
-// anwsersF.textContent = question1.choice2;
-// anwsersT.textContent = question1.choice3;
-// // function questions()
+var questionArray = ["What is a script refer to in JavaScript?",
+    "What does 'concat' mean in JavaScript?", "What answer would you expect when using length method?","To generate a random number between 0 and 1 which MathObject would you use?", "Which of the following is considered a data type?","Document Object Model, DOM refers to what?","charAt() would refer to what information?", "==, !==, >=, < are called what in JavaScript?","In a for loop, what are the three condition statements the loop consists of?"];
 
-// // for (let i = 0; i < question1.length; i++) {
-// //     question1[i];
-// //     var nextQ = question1.push(quest1);
-// //     quest.textContent = question1.push(quest1);
+var answerArray = [
+    {
+        A: "series of instructions",
+        B: "words on the screen",
+        C: "dialogue for the browser"
+    
+
+    },
+    {
+        A: "the distance between 2 points",
+        B: "to subtract",
+        C: "to put together"
+
+    },
+    {
+        A: "the distance between 2 points",
+        B: "to subtract",
+        C: "to put together"
+    }
+];
+
+    
+
+ 
+console.log(answerArray[0].A);
+
+// var output='';
+// for(var i=0; i < people.length; i++){
+
   
+// output += '<li>'+people[i].age+ '</li>'
+//   // console.log(people[i].name);
+
 // // }
-
-// //  console.log(question1)
-// // // for (let i = 0; i < question1.choice1.length; i++) {
-// // // (question1.choice1[i]);
-    
-    
-// // // }
-
-// // function nxtQuest(question) {
- 
-// // }
- 
-
-// var count = 0;
-// var pointScore = document.querySelector("#point");
-// // var decrementEl = document.querySelector("#decrement");
-// var countEl = document.querySelector("#count");
-
-// function myPoint(answersF, answersT) {
-//   countEl.textContent = count;
-//   let answersF = false;
-//   let answersT = true;
-
-//   if (answersT) {pointScore.addEventListener("click", function() {
-//     count++;
-//     alert("correct");
-//   setCounterText();
-// });
-
-
-//   }
-//   else if (answersF) {
-//   alert("wrong choice")
-//   }
-//   }
+// document.getElementById('people').innerHTML=output;
